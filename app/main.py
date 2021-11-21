0bf23d2c134a321f4d541ea467c02ca4909a95d4
Aunim HASSAN <aunim.hassan@gmail.com>
2021-11-21 12:23:17 +0100

    feat: more or less done

---
+++ app/main.py
@@ -0,0 +1,22 @@
+import random;
+
+n = random.randint(0, 100)
+attempts = 0
+print('Please guess the number (between 0 and 100) : ')
+userInput = int(input())
+
+while n != userInput:
+    if n > userInput:
+        print('It\'s more')
+        attempts += 1
+        userInput = int(input())
+    elif n < userInput:
+        print('It\'s less')
+        attempts += 1
+        userInput = int(input())
+
+print('Congrats! You find the number in ' + str(attempts) + ' attempt(s).')
+
+
+
+
